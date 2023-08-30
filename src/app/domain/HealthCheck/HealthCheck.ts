3e0ab7b2ff29f7939f71110445ca207d94dd9f06
MaksymHolovatyi <maksym.holovatyi.dev@gmail.com>
2023-08-30 15:21:01 +0300

    Add HealthCheck

---
+++ src/app/domain/HealthCheck/HealthCheck.ts
@@ -0,0 +1,9 @@
+import {Get, JsonController} from 'routing-controllers';
+
+@JsonController('/HealthCheck')
+export default class HealthCheck {
+  @Get()
+  async HealthCheck() {
+    return {message: 'Server running!'};
+  }
+}
--- src/app/domain/index.ts
+++ src/app/domain/index.ts
@@ -2,7 +2,8 @@ import Auth from './Auth/Auth';
 import Users from './Users/Users';
 import Profiles from './Profiles/Profiles';
 import Dashboard from './Dashboard/Dashboard';
+import HealthCheck from './HealthCheck/HealthCheck';
 
-const controllers = <any>[Auth, Users, Profiles, Dashboard];
+const controllers = <any>[Auth, Users, Profiles, Dashboard, HealthCheck];
 
-export { controllers };
+export {controllers};
