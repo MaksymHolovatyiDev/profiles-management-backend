af93630809237a08df8992e64aa9ada407dcd7a9
MaksymHolovatyi <maksym.holovatyi.dev@gmail.com>
2023-08-31 09:44:45 +0300

    Show env

--- src/app/domain/HealthCheck/HealthCheck.ts
+++ src/app/domain/HealthCheck/HealthCheck.ts
@@ -1,9 +1,16 @@
 import {Get, JsonController} from 'routing-controllers';
 
+const {KEY, PORT, DB_TEST} = process.env;
+
 @JsonController('/HealthCheck')
 export default class HealthCheck {
   @Get()
   async HealthCheck() {
-    return {message: 'Server running!'};
+    return {
+      message: 'Server running!',
+      key: KEY,
+      port: PORT,
+      db: DB_TEST,
+    };
   }
 }
