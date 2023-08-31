c96c68309ef953223f27a662c1031802fe825270
MaksymHolovatyi <maksym.holovatyi.dev@gmail.com>
2023-08-31 12:05:23 +0300

    Remove main data

--- src/app/domain/HealthCheck/HealthCheck.ts
+++ src/app/domain/HealthCheck/HealthCheck.ts
@@ -1,15 +1,12 @@
 import mongoose from 'mongoose';
 import {Get, JsonController} from 'routing-controllers';
 
-const {KEY, DB_HOST} = process.env;
-
 @JsonController('/HealthCheck')
 export default class HealthCheck {
   @Get()
   async HealthCheck() {
     return {
       message: 'Server running!',
-      mainData: `data ${KEY} ${DB_HOST}`,
       mongooseConnection: mongoose.connection.readyState,
     };
   }
