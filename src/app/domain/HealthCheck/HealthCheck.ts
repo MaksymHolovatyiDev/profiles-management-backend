67f3706ebdd1d30b023475aae41a322f391a320d
MaksymHolovatyi <maksym.holovatyi.dev@gmail.com>
2023-08-31 11:15:52 +0300

    Hide env vars

--- src/app/domain/HealthCheck/HealthCheck.ts
+++ src/app/domain/HealthCheck/HealthCheck.ts
@@ -1,16 +1,11 @@
 import {Get, JsonController} from 'routing-controllers';
 
-const {KEY, PORT, DB_TEST} = process.env;
-
 @JsonController('/HealthCheck')
 export default class HealthCheck {
   @Get()
   async HealthCheck() {
     return {
-      message: 'Server running!!!',
-      key: `key ${KEY}`,
-      port: PORT,
-      db: DB_TEST,
+      message: 'Server running!',
     };
   }
 }
