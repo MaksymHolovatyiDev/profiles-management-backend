3e5860514256f937da0461883b6e98a4e5c27bb8
MaksymHolovatyi <maksym.holovatyi.dev@gmail.com>
2023-08-31 11:40:59 +0300

    Add main data

--- src/app/domain/HealthCheck/HealthCheck.ts
+++ src/app/domain/HealthCheck/HealthCheck.ts
@@ -1,12 +1,15 @@
 import mongoose from 'mongoose';
 import {Get, JsonController} from 'routing-controllers';
 
+const {KEY, DB_HOST} = process.env;
+
 @JsonController('/HealthCheck')
 export default class HealthCheck {
   @Get()
   async HealthCheck() {
     return {
       message: 'Server running!',
+      mainData: `data ${KEY} ${DB_HOST}`,
       mongooseConnection: mongoose.connection.readyState,
     };
   }
